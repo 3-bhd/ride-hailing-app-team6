1ce353d9c34403f768e7724b86738acd312d2281
3-bhd <omarabdelhady@aucegypt.edu>
2025-12-06 06:30:36 +0200

    Added the needed files and modifications for deployment

--- app.py
+++ app.py
@@ -1178,4 +1178,6 @@ def driver_requests():
 # RUN
 # ===============================
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
